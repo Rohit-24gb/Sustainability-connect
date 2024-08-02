495cca880947cf68719a9d8444a2f5916428e493
Rohit-24gb <rohitrajaksnd@gmail.com>
2024-08-02 22:10:32 +0530

    recycleableitems

--- sus-app/src/components/RecycleableItems/RecycleableItems.js
+++ sus-app/src/components/RecycleableItems/RecycleableItems.js
@@ -8,7 +8,7 @@ const RecyclableItems = () => {
     // Fetch data from the backend API
     const fetchData = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/recyclable-items`);
+        const response = await fetch('http://localhost:4000/api/recyclable-items');
         const data = await response.json();
         setItems(data);
       } catch (error) {
@@ -38,5 +38,54 @@ const RecyclableItems = () => {
 };
 
 export default RecyclableItems;
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import './RecycleableItems.css';
+
+// const RecyclableItems = () => {
+//   const [items, setItems] = useState([]);
+
+//   useEffect(() => {
+//     // Fetch data from the backend API
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/recyclable-items`);
+//         const data = await response.json();
+//         setItems(data);
+//       } catch (error) {
+//         console.error('Error fetching recyclable items:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []); 
+
+//   return (
+//     <div className="section items-container">
+//       <h2>Popularly Recyclable Items</h2>
+//       <div className="items">
+//         <div className="items-content">
+//           {items.map(item => (
+//             <div key={item.id} className="item-card">
+//               <img src={item.image} alt={item.name} />
+//               <h3>{item.name}</h3>
+//               <p>{item.description}</p>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RecyclableItems;
 
 
