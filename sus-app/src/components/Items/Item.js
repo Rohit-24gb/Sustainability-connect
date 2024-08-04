27613fa2ce95470b3ba28acba0dfec68a591a0e5
Rohit Kumar Rajak <72152718+Rohit-24gb@users.noreply.github.com>
2024-08-04 21:52:36 +0530

    Update Item.js code updated

--- sus-app/src/components/Items/Item.js
+++ sus-app/src/components/Items/Item.js
@@ -1,25 +1,21 @@
-import React from "react";
-import './Item.css'
-import { Link } from "react-router-dom";
-const Item = (props) => {
-   return (
-      <div className="item">
-         <Link to={`/product/${props.id}`}>
-            <img onClick={window.scrollTo(0, 0)}
-               src={props.image} /></Link>
-         <p>{props.name}</p>
-         <div className="item-price">
-            <div className="item-price-new">
-               ${props.new_price}
-            </div>
-            <div className="item-price-old">
-               $ {props.old_price}
+import React from 'react';
+import './Item.css';
+import { Link } from 'react-router-dom';
 
-            </div>
-         </div>
+const Item = ({ id, name, image_url, price, onClick }) => {
+  return (
+    <div className="item" onClick={onClick}>
+      <Link to={`/product/${id}`} onClick={(e) => e.stopPropagation()}>
+        <img src={image_url} alt={name} />
+      </Link>
+      <p>{name}</p>
+      <div className="item-price">
+        <div className="item-price-new">
+          ${price}
+        </div>
       </div>
-   )
+    </div>
+  );
+};
 
-
-}
-export default Item
+export default Item;
