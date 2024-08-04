fc571830a51c5f2c0c214322a9e438ffc911e7a3
Rohit Kumar Rajak <72152718+Rohit-24gb@users.noreply.github.com>
2024-08-05 00:11:05 +0530

    Update ProductCard.js price sign change

--- sus-app/src/components/ProductCard/ProductCard.js
+++ sus-app/src/components/ProductCard/ProductCard.js
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
       <img src={product.image_url} alt={product.name} />
       <div className="product-card-content">
         <h3 className="product-card-title">{product.name}</h3>
-        <p className="product-card-price">${product.price.toFixed(2)}</p>
+        <p className="product-card-price">₹ {product.price.toFixed(2)}</p>
         {/* <p className="product-card-description">{product.description}</p> */}
       </div>
     </div>
